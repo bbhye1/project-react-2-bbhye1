ecbdfa4c7a5291bcb2a64bec3ab2a773c5e7d838
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-11-20 16:57:13 +0900

    Change MoodControllerPage.
    Add MoodEnergyForm and MoodBrightnessForm.
    And delete MoodController

---
+++ src/styles/Form.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const Form = styled.form({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: '20px',
+});
+
+export default Form;
